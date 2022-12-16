24c8c357fd81dce4c1dea537ee9b692e3df5657d
Amal <almaabadyamal@gmail.com>
2022-12-16 23:15:15 +0300

    Initial Commit

---
+++ CRUDActivity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CRUDActivity
+//
+//  Created by Eman on 22/05/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
